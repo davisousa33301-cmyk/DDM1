1c3de8115a1ef71fabd7609a77d768ffd7e2b0fb
Davi Silva <davisousa33301@gmail.com>
2026-09-18 19:58:46 -0300

    Refactor tela-cadastro.jsx for improved styling and structure

--- Frost Guardians/tela-cadastro.jsx
+++ Frost Guardians/tela-cadastro.jsx
@@ -1,103 +1,117 @@
-import { View, Image, Text, TextInput, Button } from 'react-native';
+import { View, Image, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 export default function App() {
-    return (
-        <View style={{ flex: 1, backgroundColor: '#4682b4' }}>
-            <View style={{ flex: 1, backgroundColor: '#4682b4' }}>
-                <Image
-                    source={{
-                        uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRpX8SI8VcFSSSWKFmIhC51oJUp6jkrQWmzyUeG1h-8A&s=10',
-                    }}
-                    style={{
-                        width: 100,
-                        height: 100,
-                        alignSelf: 'center',
-                        marginTop: 50,
-                        borderRadius: 50,
-                    }}
-                />
-                <Text
-                    style={{
-                        fontWeight: 'bold',
-                        fontSize: 30,
-                        textAlign: 'center',
-                        color: '#ffffff',
-                    }}>
-                    CADASTRO
-                </Text>
-                <Text style={{ color: '#ffffff' }}> Insira seu nome completo: </Text>
-                <TextInput
-                    placeholder="Digite seu nome aqui"
-                    style={{
-                        borderRadius: 5,
-                        borderWidth: 1,
-                        margin: 10,
-                        padding: 5,
-                        color: '#828282',
-                        borderColor: '#bfddf3',
-                        backgroundColor: '#ffffff',
-                    }}
-                />
+  return (
+    <View style={tela_cad.principal}>
+      <View style={tela_cad.conteudo}>
+        <Image source={require('./1.png')} style={tela_cad.logo} />
 
-                <Text style={{ color: '#ffffff' }}> Insira seu telefone: </Text>
-                <TextInput
-                    placeholder="Digite seu telefone aqui"
-                    keyboardType="numeric"
-                    style={{
-                        borderRadius: 5,
-                        borderWidth: 1,
-                        margin: 10,
-                        padding: 5,
-                        color: '#828282',
-                        borderColor: '#bfddf3',
-                        backgroundColor: '#ffffff',
-                    }}
-                />
+        <Text style={tela_cad.titulo}>CADASTRO</Text>
 
-                <Text style={{ color: '#ffffff' }}> Insira seu e-mail: </Text>
-                <TextInput
-                    placeholder="Digite seu e-mail aqui"
-                    style={{
-                        borderRadius: 5,
-                        borderWidth: 1,
-                        margin: 10,
-                        padding: 5,
-                        color: '#828282',
-                        borderColor: '#bfddf3',
-                        backgroundColor: '#ffffff',
-                    }}
-                />
+        <Text style={tela_cad.informacao}>   Insira seu nome completo: </Text>
 
-                <Text style={{ color: '#ffffff' }}> Insira sua senha: </Text>
-                <TextInput
-                    placeholder="Digite sua senha aqui"
-                    keyboardType="numeric"
-                    secureTextEntry={true}
-                    style={{
-                        borderRadius: 5,
-                        borderWidth: 1,
-                        margin: 10,
-                        padding: 5,
-                        color: '#828282',
-                        borderColor: '#bfddf3',
-                        backgroundColor: '#ffffff',
-                    }}
-                />
+        <TextInput
+          placeholder=" Digite seu nome aqui"
+          placeholderTextColor="#828282"
+          style={tela_cad.input}
+        />
 
-                <Button
-                    title="Enviar"
-                    onPress={() => {
-                        alert('Enviado!');
-                    }}
-                /* para desativar o botão: disabled = { true } */
-                />
-            </View>
-            <View style={{ flex: 0.1, backgroundColor: '#bfddf3' }}>
-                <Text style={{ textAlign: 'center', marginTop: 9, color: '#4682b4' }}>
-                    © 2026 Davi Silva - Frost Guardians — Todos os direitos reservados -
-                    v.1.0
-                </Text>
-            </View>
-        </View>
-    );
+        <Text style={tela_cad.informacao}>   Insira seu telefone:</Text>
+
+        <TextInput
+          placeholder=" Digite seu telefone aqui"
+          placeholderTextColor="#828282"
+          keyboardType="numeric"
+          style={tela_cad.input}
+        />
+
+        <Text style={tela_cad.informacao}>   Insira seu e-mail:</Text>
+
+        <TextInput
+          placeholder=" Digite seu e-mail aqui"
+          placeholderTextColor="#828282"
+          keyboardType="email-address"
+          style={tela_cad.input}
+        />
+
+        <Text style={tela_cad.informacao}>   Insira sua senha:</Text>
+
+        <TextInput
+          placeholder=" Digite sua senha aqui"
+          placeholderTextColor="#828282"
+          secureTextEntry={true}
+          style={tela_cad.input}
+        />
+
+        <Button
+          title="Enviar"
+          onPress={() => {
+            alert('Enviado!');
+          }}
+        />
+      </View>
+
+      <View style={tela_cad.rodape}>
+        <Text style={tela_cad.texto_rodape}>
+          © 2026 Davi Silva - Frost Guardians — Todos os direitos reservados -
+          v.1.0
+        </Text>
+      </View>
+    </View>
+  );
 }
+
+const tela_cad = StyleSheet.create({
+  principal: {
+    flex: 1,
+    backgroundColor: '#121212',
+  },
+
+  conteudo: {
+    flex: 1,
+    backgroundColor: '#121212',
+  },
+
+  logo: {
+    width: 100,
+    height: 100,
+    alignSelf: 'center',
+    marginTop: 15,
+    borderRadius: 50,
+  },
+
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+    color: '#ffffffbf',
+    margin: 10,
+  },
+
+  informacao: {
+    color: '#ffffff',
+    padding: 4,
+  },
+
+  input: {
+    borderRadius: 10,
+    borderWidth: 2,
+    margin: 10,
+    padding: 5,
+    color: '#828282',
+    borderColor: '#bfddf3',
+    backgroundColor: '#121212',
+  },
+
+  rodape: {
+    flex: 0.1,
+    backgroundColor: '#bfddf3',
+    justifyContent: 'center',
+  },
+
+  texto_rodape: {
+    textAlign: 'center',
+    marginTop: 9,
+    color: '#4682b4',
+  },
+});
